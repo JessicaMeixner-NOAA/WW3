7447a3ab1014155b2e56645e03e28e28c5890fbe
Jessica Meixner <Jessica.Meixner@noaa.gov>
2025-04-29 18:09:39 +0000

    bug fix

--- model/src/wminitmd.F90
+++ model/src/wminitmd.F90
@@ -2357,7 +2357,7 @@ CONTAINS
            ODAT(:,I),                          &
            FLGRD(:,:,I),FLGR2(:,:,I),FLGD(:,I),FLG2(:,I),  &
            OT2(I)%NPTS, OT2(I)%X, OT2(I)%Y, OT2(I)%PNAMES, &
-           IPRT(:,I), LPRT(I), MPI_COMM_LOC, CROOT)
+           IPRT(:,I), LPRT(I), MPI_COMM_LOC, CROOT-1)
 #else 
        CALL W3INIT ( I, .TRUE., MNAMES(I), MDS(:,I), NTRACE(:,I),    &
            ODAT(:,I),                          &
